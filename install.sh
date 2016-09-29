39684256c0e26058c097beadeea7b21fe631eede
Kun Feng <fkengun@gmail.com>
2016-09-28 21:03:57 -0500

    Use -f instead of -a to check files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo Installing Chameleon Toolkit ...
 
 # Install SSH key
-if [ -a keys/id_rsa ] && [ -a keys/id_rsa.pub ]
+if [ -f keys/id_rsa ] && [ -f keys/id_rsa.pub ]
 then
   cp keys/id_rsa ~/.ssh/id_rsa
   chmod 600 ~/.ssh/id_rsa
