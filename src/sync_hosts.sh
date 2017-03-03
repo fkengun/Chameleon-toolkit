0f52e37d0819d3d3bc9bee38a64e1b4041648bad
Kun Feng <fkengun@gmail.com>
2017-03-03 03:33:46 +0000

    Remove the requirement of MPSSH

--- src/sync_hosts.sh
+++ src/sync_hosts.sh
@@ -83,4 +83,12 @@ done
 wait
 
 echo "Synchronizing ..."
-mpssh -bf ~/nodes "sudo mv /home/cc/hosts /etc/hosts" > /dev/null 2>&1
+if mpssh > /dev/null 2>&1
+then
+  mpssh -bf ~/nodes "sudo mv /home/cc/hosts /etc/hosts" > /dev/null 2>&1
+else
+  for host in ${HOSTS[@]}
+  do
+    ssh $host "sudo mv /home/cc/hosts /etc/hosts"
+  done
+fi
