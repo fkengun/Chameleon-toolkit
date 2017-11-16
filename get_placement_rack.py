08b712e13209ff64550816299209befd19fc16a9
Kun Feng <fkengun@gmail.com>
2017-11-16 02:44:55 +0000

    Use host list to get host id before querying detailed info

--- get_placement_rack.py
+++ get_placement_rack.py
@@ -3,9 +3,9 @@ import subprocess
 import collections
 import multiprocessing as mp
 
-CLIENT_HOME = "/home/cc/blazarclient"
-MIN_HOST_ID = 1
-MAX_HOST_ID = 1024
+client_home = "/home/cc/blazarclient"
+output_file = "./id.mac.rack"
+step = mp.cpu_count() / 2
 
 class Node:
   id = ""
@@ -18,29 +18,39 @@ class Node:
     self.rack = rack
 
   def __str__(self):
-    return "%4d\t\t%s\t\t%s" % (self.id, self.mac, self.rack)
+    if not self.mac:
+      self.mac = "\t\t\t"
+    if not self.rack:
+      self.rack = "\t"
+    return "%s\t\t%s\t\t%s" % (self.id, self.mac, self.rack)
+
+def get_node_list():
+  command = "(%s/bin/climate host-list 2>&1) | grep \"|\" | grep -v cpu | awk '{print $2}' | sort -n" % (client_home)
+  process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+  output, error = process.communicate()
+  node_list = output.splitlines()
+  return node_list
 
 def get_mac_rack_by_id(id, nodes):
-  command = "(%s/bin/climate host-show %s 2>&1) | grep \"network_adapters.0.mac\" | awk '{print $4}'" % (CLIENT_HOME, id)
+  command = "(%s/bin/climate host-show %s 2>&1) | grep \"network_adapters.0.mac\" | awk '{print $4}'" % (client_home, id)
   process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
   mac, error = process.communicate()
-  command = "(%s/bin/climate host-show %s 2>&1) | grep \"placement.rack\" | awk '{print $4}'" % (CLIENT_HOME, id)
+  command = "(%s/bin/climate host-show %s 2>&1) | grep \"placement.rack\" | awk '{print $4}'" % (client_home, id)
   process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
   rack, error = process.communicate()
-  if mac:
-    node = Node(id, mac.rstrip(), rack.rstrip())
-    nodes[id] = node
+  node = Node(id, mac.rstrip(), rack.rstrip())
+  nodes[id] = node
 
 if __name__ == "__main__":
-  if os.path.isfile("./id.mac.rack"):
-    os.remove("./id.mac.rack")
+  if os.path.isfile(output_file):
+    os.remove(output_file)
 
-  step = 12
-  step_count = (MAX_HOST_ID - MIN_HOST_ID) / step
+  node_list = get_node_list()
+  step_count = len(node_list) / step
+  manager = mp.Manager()
+  nodes = manager.dict()
   for s in range(0, step_count):
-    ids = range(MIN_HOST_ID + s * step, min(MAX_HOST_ID, MIN_HOST_ID + (s+1) * step))
-    manager = mp.Manager()
-    nodes = manager.dict()
+    ids = node_list[s*step : (s+1)*step]
     jobs = []
     for i in ids:
       p = mp.Process(target=get_mac_rack_by_id, args=(i, nodes))
@@ -50,8 +60,10 @@ if __name__ == "__main__":
     for proc in jobs:
       proc.join()
 
-    with open('./id.mac.rack', 'a') as f:
-      for node in nodes.values():
-        f.write("%s\n" % node.__str__())
-      f.flush()
-      f.close()
+  with open('./id.mac.rack', 'a') as f:
+    for node in nodes.values():
+      f.write("%s\n" % node.__str__())
+    f.flush()
+    f.close()
+
+  print "host id, MAC and rack id have been stored in %s\n" % output_file
